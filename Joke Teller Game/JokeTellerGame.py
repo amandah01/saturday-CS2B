d6bafe44a5f901296f26ce3e8a9ab4f413e92dc9
amandah01 <32213166+amandah01@users.noreply.github.com>
2017-10-21 15:30:01 -0700

    Update and rename joketeller/jokes.py to Joke Teller Game/JokeTellerGame.py

---
+++ Joke Teller Game/JokeTellerGame.py
@@ -0,0 +1,86 @@
+# Joke Teller Game
+# A game created by Amanda Huynh
+# Sept 30, 2017
+
+# This game tells you some cool jokes.
+
+# All jokes were stolen from the internet.
+
+# Double Quotes
+print("What do cows like to read in their leisure time?")
+input()
+print("Cattle-logs")
+print("***HAHAHA***")
+print()
+
+# Single Quotes
+print('To all those Harry Potter fans...')
+print('What type of programming is Harry Potter very good at?')
+input()
+print('Python')
+print('***GET ITTT***')
+print()
+
+print("What do you do with a sick boat?")
+input()
+print("You take it to the doc.")
+print("***LOLOLLLLL***")
+print()
+
+print("\"What do you call a bee that's having a bad hair day?\"")
+input()
+print("Frisbee")
+print("***BUZZINNN'***")
+print()
+
+print("\"Why wouldn’t the shrimp share his treasure?\"")
+input()
+print("He was shellfish")
+print("***HAHAA***")
+print()
+
+print("What do you do if someone thinks watching the total solar eclipse without glasses is the only thing that can make him/her go blind?")
+input()
+print("Show him/her a picture of yourself")
+print("***HAHAA JKJK***")
+print()
+
+print("Why was your mother disappointed when you learned C++?")
+input()
+print("She wanted you to learn A++.")
+print("***XXDDDDDD***")
+print()
+
+print()
+print("What did the rubber band factory worker say when he was fired?", end="")
+input()
+print("Oh, snap!")
+print("***AYYEEE***")
+print()
+
+print("Why are frogs are so happy?", end="")
+input()
+print("They eat whatever bugs them.")
+print("***DAYYUMMMM***")
+print()
+
+print('Why did the old lady fall into the well?', end='')
+input()
+print('She didn\'t see it that well!') 
+print("***LOLOLOLOLOLOLOLOLLOOLOLOL***")
+print()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
